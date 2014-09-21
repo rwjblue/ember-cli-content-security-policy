69acb837ae2814a4c00457084d9f1832accc3deb
Robert Jackson <robert.w.jackson@me.com>
2014-09-20 22:04:29 -0400

    Move config into addon itself (easier to grok).

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 module.exports = {
-  name: 'ember-cli-content-security-policy'
+  name: 'ember-cli-content-security-policy',
+
+  config: function(/* environment, appConfig */) {
+    return {
+      contentSecurityPolicyHeader: 'Content-Security-Policy',
+      contentSecurityPolicy: {
+        'default-src': 'none',
+        'script-src': 'self',
+        'connect-src': 'self',
+        'img-src': 'self',
+        'style-src': 'self'
+      }
+    }
+  }
 };
