08db7ab834403d1718799d208f14dd4c830d69d3
Robert Jackson <robert.w.jackson@me.com>
2014-09-20 22:04:47 -0400

    Add middleware.

--- index.js
+++ index.js
@@ -12,5 +12,39 @@ module.exports = {
         'style-src': 'self'
       }
     }
+  },
+
+  serverMiddleware: function(config) {
+    var addonContent = this;
+    var app = config.app;
+    var options = config.options;
+    var project = options.project;
+
+    app.use(function(req, res, next) {
+      var appConfig = project.config(config.environment);
+
+      var header = appConfig.contentSecurityPolicyHeader;
+      var headerConfig = appConfig.contentSecurityPolicy;
+
+      if (options.liveReload) {
+        headerConfig['connect-src'] = headerConfig['connect-src'] + ' localhost:' + options.liveReloadPort;
+      }
+
+      var headerValue = Object.keys(headerConfig).reduce(function(memo, value) {
+        return memo + '; ' + value + ' ' + headerConfig[value] + '; ';
+      }, '');
+
+      if (!header || !headerValue) {
+        next();
+        return;
+      }
+
+      res.removeHeader("Content-Security-Policy");
+      res.removeHeader('Content-Security-Policy-Report-Only');
+
+      res.setHeader(header, headerConfig);
+
+      next();
+    });
   }
 };
