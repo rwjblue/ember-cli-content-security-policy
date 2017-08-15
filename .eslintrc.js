f187d7638be18882f5d2495f4ddab77796979c8e
Alexander Sandström <mail+github@a16m.se>
2017-08-15 09:56:44 +0200

    ESlint adjustments

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,5 +9,14 @@ module.exports = {
     browser: true
   },
   rules: {
-  }
+    // disallow declaration of variables that are not used in the code
+    'no-unused-vars': [
+      'error',
+      {
+        varsIgnorePattern: '^_',
+        argsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+  },
 };
