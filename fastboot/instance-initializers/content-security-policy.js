1fe117fb4adce636c4394c433698eaed8ef3044b
Jeldrik Hanschke <admin@jhanschke.de>
2019-10-06 22:45:02 +0200

    instance-initializer should only be present in fastboot builds

--- fastboot/instance-initializers/content-security-policy.js
+++ fastboot/instance-initializers/content-security-policy.js
@@ -1,1 +1,34 @@
-export { default, initialize } from 'ember-cli-content-security-policy/instance-initializers/content-security-policy';
+import { assert } from '@ember/debug';
+
+// reads addon config stored in meta element
+function readAddonConfig(appInstance) {
+  let config = appInstance.resolveRegistration('config:environment');
+  let addonConfig = config['ember-cli-content-security-policy'];
+
+  // TODO: do not require policy to be stored in config object
+  //       if already available through CSP meta element
+  assert(
+    'Required configuration is available at run-time',
+    addonConfig.hasOwnProperty('reportOnly') && addonConfig.hasOwnProperty('policy')
+  );
+
+  return config['ember-cli-content-security-policy'];
+}
+
+export function initialize(appInstance) {
+  let fastboot = appInstance.lookup('service:fastboot');
+
+  if (!fastboot || !fastboot.get('isFastBoot')) {
+    // nothing to do if application does not run in FastBoot or
+    // does not even have a FastBoot service
+    return;
+  }
+
+  let { policy, reportOnly } = readAddonConfig(appInstance);
+  let header = reportOnly ? 'Content-Security-Policy-Report-Only' : 'Content-Security-Policy';
+  fastboot.get('response.headers').set(header, policy);
+}
+
+export default {
+  initialize
+};
