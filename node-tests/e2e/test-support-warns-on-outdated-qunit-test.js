c5210827794329851dc34ac0a309f2ade01eff16
Jeldrik Hanschke <jelhan@users.noreply.github.com>
2021-10-28 01:16:53 +0200

    Run scenarios with expected Ember CLI version (#202)

--- node-tests/e2e/test-support-warns-on-outdated-qunit-test.js
+++ node-tests/e2e/test-support-warns-on-outdated-qunit-test.js
@@ -1,6 +1,7 @@
 const expect = require('chai').expect;
 const TestProject = require('ember-addon-tests').default;
 const path = require('path');
+const { readPackageJson } = require('../utils');
 
 describe('e2e: test support warns if dependencies are not supported', function () {
   this.timeout(300000);
@@ -11,9 +12,24 @@ describe('e2e: test support warns if dependencies are not supported', function (
     });
 
     await testProject.createEmberApp();
+
+    // ember-qunit was upgraded to 5.x and qunit added as a direct dependency
+    // in Ember CLI 3.24.0. This test can not be performed for such new Ember
+    // CLI versions.
+    const packageJson = await readPackageJson(testProject);
+    const hasQunitAsDirectDependency = Object.keys(
+      packageJson.devDependencies
+    ).includes('qunit');
+    if (hasQunitAsDirectDependency) {
+      return;
+    }
+
     await testProject.addOwnPackageAsDevDependency(
       'ember-cli-content-security-policy'
     );
+
+    // Ensure that projects uses an old ember-qunit version, which depends on
+    // incompatible qunit version.
     await testProject.addDevDependency('ember-qunit', '4.0.0');
 
     try {
